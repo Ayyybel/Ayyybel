812ccaeebaaa1df6fad4543b8449b365d265ef1a
Ayyybel <Ayyybel@users.noreply.github.com>
2025-08-14 22:27:14 +0000

    deploy: 5073da10271a02054dd03b33b2f47b20d79d1885

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Reflections on a year of biking to work",
+        "excerpt":"Starting in July 2023, I biked to work five days a week, for about 2 miles each way. That comes out to somewhere around 1000 miles, maybe a bit less accounting for sick days and incidental and occasional (but appreciated) rides from my girlfriend. With a total of two bikes...","categories": [],
+        "tags": [],
+        "url": "/reflections-on-a-year-of-biking-to-work/",
+        "teaser": null
+      },{
         "title": "Altar to organization",
         "excerpt":"Your clean clothes are migrating from chair to bed to chair and your life is in shambles. You lose your phone, your wallet, and your jewelry. You frantically clean before every unsuccessful date. You’ve always thought of an organized room as the minimalist, sanitized, color-coordinated ones you see on Pinterest....","categories": [],
         "tags": [],
@@ -9,5 +15,11 @@ var store = [{
         "excerpt":"It’s a little bit macabre that someone my age, 23, is thinking about legacy with much intensity. It should be a topic for the elderly, the older, and those who’ve accomplished something. One should begin thinking about legacy in their donations to colleges, not as they graduate from them. It’s...","categories": [],
         "tags": [],
         "url": "/legacy-for-the-hopelessly-ambitious/",
+        "teaser": null
+      },{
+        "title": "Blog rules",
+        "excerpt":"This is the set of rules I am making for my own personal blog. I think they serve a useful purpose in preserving and protecting the things I write from my future self, as well as creating guidelines to facilitate creating more. These are in no particular order. All rules...","categories": [],
+        "tags": [],
+        "url": "/blog-rules/",
         "teaser": null
       }]
