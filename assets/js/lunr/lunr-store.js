1c149dabcf22146541eabb24c7c0f65c2446508f
Ayyybel <Ayyybel@users.noreply.github.com>
2025-10-01 06:34:28 +0000

    deploy: cc21bd86d9536b8b48042c700359b0a23a293357

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,31 +1,31 @@
 var store = [{
         "title": "Reflections on a year of biking to work",
         "excerpt":"Starting in July 2023, I biked to work five days a week, for about 2 miles each way. That comes out to somewhere around 1000 miles, maybe a bit less accounting for sick days and incidental and occasional (but appreciated) rides from my girlfriend. With a total of two bikes...","categories": [],
-        "tags": [],
+        "tags": ["writing"],
         "url": "/reflections-on-a-year-of-biking-to-work/",
         "teaser": null
       },{
         "title": "Altar to organization",
         "excerpt":"Your clean clothes are migrating from chair to bed to chair and your life is in shambles. You lose your phone, your wallet, and your jewelry. You frantically clean before every unsuccessful date. You’ve always thought of an organized room as the minimalist, sanitized, color-coordinated ones you see on Pinterest....","categories": [],
-        "tags": [],
+        "tags": ["writing"],
         "url": "/altar-to-organization/",
         "teaser": null
       },{
         "title": "Blog rules",
         "excerpt":"I think this blog will have 2 main kinds of posts: essays and updates. Essays will follow the rules below, while updates are more casual, basically just Instagram posts. This is the set of rules I am making for my own personal blog. I think they serve a useful purpose...","categories": [],
-        "tags": [],
+        "tags": ["writing"],
         "url": "/blog-rules/",
         "teaser": null
       },{
         "title": "Adventures",
         "excerpt":"There are a few different kinds of adventures. I think there are 3. But first, what makes an outing an adventure? I think it has to do with the aspect of a challenge. It’s got to have some kind of goal that you aren’t sure you can achieve. It’s not...","categories": [],
-        "tags": [],
+        "tags": ["writing"],
         "url": "/adventures/",
         "teaser": null
       },{
         "title": "Mourn while it matters",
         "excerpt":"I think we should always be in mourning. Not for things that have passed, but for the things we have. If we are aware of their eventual passing, and remind ourselves of it occasionally, it makes us appreciate the things and people around us so much more. The most painful...","categories": [],
-        "tags": [],
+        "tags": ["writing"],
         "url": "/mourn-while-it-matters/",
         "teaser": null
       }]
